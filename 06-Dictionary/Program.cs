64a3c82c92dfe3d49e13493a917b476012984acb
zawmintun <mintun.zaw@merry.com.sg>
2022-10-26 17:31:15 +0800

    Dictionary

---
+++ 06-Dictionary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<String, String> dTest = new Dictionary<String, String>();
+            dTest["Test"] = "Try to do an activity to know what the outcome will be";
+            dTest["Home"] = "An accommodation where a group of people stays together";
+            dTest["Apple"] = "A fruit which was eaten by Eve";
+            dTest["Game"] = "Something to play for a person or a group of people";
+            Console.WriteLine("Printing the dictionary");
+            Console.WriteLine("=======================");
+            foreach (KeyValuePair<String, String> entry in dTest)
+            {
+                Console.WriteLine(entry.Key + ": " + entry.Value);
+            }
+            Console.WriteLine(Environment.NewLine);
+            SortedDictionary<String, String> dTestSorted = new SortedDictionary<String, String>(dTest);
+            Console.WriteLine("Printing the dictionary in order");
+            Console.WriteLine("================================");
+            foreach (KeyValuePair<String, String> entry in dTestSorted)
+            {
+                Console.WriteLine(entry.Key + ": " + entry.Value);
+            }
+        }
+    }
+}
