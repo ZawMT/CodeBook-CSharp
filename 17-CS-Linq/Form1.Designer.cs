4fcba75fb3345b3350ae52b4b068b7bb12f03522
zawmintun <mg.zawmintun@gmail.com>
2023-03-22 17:14:17 +0800

    Linq - Sort

---
+++ 17-CS-Linq/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace _17_CS_Linq
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSort = new System.Windows.Forms.Button();
+            this.txtTrace = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(724, 253);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(112, 34);
+            this.btnSort.TabIndex = 0;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // txtTrace
+            // 
+            this.txtTrace.Location = new System.Drawing.Point(12, 12);
+            this.txtTrace.Multiline = true;
+            this.txtTrace.Name = "txtTrace";
+            this.txtTrace.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtTrace.Size = new System.Drawing.Size(678, 275);
+            this.txtTrace.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 299);
+            this.Controls.Add(this.txtTrace);
+            this.Controls.Add(this.btnSort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.TextBox txtTrace;
+    }
+}
+
---
+++ 17-CS-Linq/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace _17_CS_Linq
+{
+    public partial class Form1 : Form
+    {
+        class Human
+        {
+            private DateTime dtDOB;
+            public DateTime DOB { get { return dtDOB; } set { dtDOB = value; } }
+            private String strName;
+            public String Name { get { return strName; } set { strName = value; } }
+            public int AgeInYears 
+            { 
+                get 
+                {
+                    DateTime dtNow = DateTime.Now;
+                    int iYr = dtNow.Year - dtDOB.Year;
+                    if (dtNow.Month > dtDOB.Month)
+                        return iYr;
+
+                    if (dtNow.Month == dtDOB.Month && dtNow.Day >= dtDOB.Day)
+                        return iYr;
+
+                    return iYr - 1;
+                } 
+            }
+
+            public int AgeInDays
+            {
+                get
+                {
+                    DateTime dtNow = DateTime.Now;
+                    TimeSpan ts = dtNow - dtDOB;
+                    return (int)ts.TotalDays;
+                }
+            }
+
+            public Human(string strName, DateTime dtDOB)
+            {
+                this.strName = strName;
+                this.dtDOB = dtDOB;
+            }
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            Human p1 = new Human("John", new DateTime(1990, 1, 2));
+            Human p2 = new Human("Mary", new DateTime(1970, 2, 2));
+            Human p3 = new Human("Jane", new DateTime(1979, 3, 27));
+            Human p4 = new Human("Clive", new DateTime(2023, 1, 2));
+            Human p5 = new Human("Don", new DateTime(2023, 2, 3));
+            List<Human> lstP = new(new Human[] { p1, p2, p3, p4, p5 });
+            WriteInTrace("Print the original list");
+            fnPrint(lstP);
+            WriteInTrace("Print the list sorted by Age/Days");
+            lstP.Sort((x, y) => x.AgeInDays.CompareTo(y.AgeInDays));
+            fnPrint(lstP);
+            WriteInTrace("Print the list sorted by Age/Years");
+            lstP.Sort((x, y) => x.AgeInYears.CompareTo(y.AgeInYears));
+            fnPrint(lstP);
+            WriteInTrace("Print the list sorted by Name");
+            lstP.Sort((x, y) => x.Name.CompareTo(y.Name));
+            fnPrint(lstP);
+        }
+
+        private void fnPrint(List<Human> lst)
+        {
+            foreach(Human h in lst)
+            {
+                if (h.AgeInYears <= 0)
+                    WriteInTrace($"Name: {h.Name}, DOD: {h.DOB}, Age: {h.AgeInDays} day(s)");
+                else
+                    WriteInTrace($"Name: {h.Name}, DOD: {h.DOB}, Age: {h.AgeInYears} year(s)");
+            }
+        }
+
+        private void WriteInTrace(string str)
+        {
+            txtTrace.Text = txtTrace.Text + Environment.NewLine + str;
+        }
+    }
+}
